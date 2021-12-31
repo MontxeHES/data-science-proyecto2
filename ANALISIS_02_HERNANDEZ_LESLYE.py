2d4af49c4f88c06ccdedcdc7ed8f9a0615413c7c
MontxeHES <95303702+MontxeHES@users.noreply.github.com>
2021-12-31 03:02:06 +0000

    Entregable Final

--- ANALISIS_02_HERNANDEZ_LESLYE.py
+++ ANALISIS_02_HERNANDEZ_LESLYE.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import math
 
 df = pd.read_csv("synergy_logistics_database.csv")
 df["route"] = df[["origin", "destination"]].apply(lambda x: "-".join(x), axis=1)
@@ -9,41 +10,45 @@ df["country"] = df[["origin", "destination", "direction"]].apply(lambda x: x[1] 
 #print(df.shape)
 #print(df.describe())
 
+
+def top_values(
+            df, 
+            top, 
+            group_column, 
+            acc_column,
+            filter_column=None, 
+            filter_value=None
+):
+    if filter_column and filter_value:
+        df =  df[df[filter_column] == filter_value]
+    df_total = df.groupby(group_column)[[acc_column]].sum().reset_index()
+    return df_total.nlargest(top, acc_column)
+
+def market_share(
+                df, 
+                group_column, 
+                acc_column, 
+                percent
+):
+    df_sorted = df.groupby(group_column)[acc_column].sum().sort_values(ascending=False).reset_index()
+    df_sorted["cum_percent"] = 100*(df_sorted[acc_column].cumsum()/df_sorted[acc_column].sum())
+    df_sorted["cum_percent"] = df_sorted["cum_percent"].apply(lambda x: math.ceil(x / 10) * 10)
+    return df_sorted[df_sorted["cum_percent"] <= percent]
+
 # Analisis opcion 1 rutas
-df_total_routes = df.groupby("route")[["total_value"]].sum().reset_index()
-df_top_routes = df_total_routes.nlargest(10, "total_value")
-
-
-df_total_routes_x = df[df["direction"] == "Exports"].groupby("route")[["total_value"]].sum().reset_index()
-df_top_routes_x = df_total_routes_x.nlargest(10, "total_value")
-
-
-df_total_routes_i = df[df["direction"] == "Imports"].groupby("route")[["total_value"]].sum().reset_index()
-df_top_routes_i = df_total_routes_i.nlargest(10, "total_value")
-#print(df_top_routes)
-#print(df_top_routes_x)
-#print(df_top_routes_i)
+df_top_routes_fn = top_values(df, 10, "route", "total_value")
+df_top_routes_fn_x = top_values(df, 10, "route", "total_value", 
+"direction", "Exports")
+df_top_routes_fn_i = top_values(df, 10, "route", "total_value", "direction", "Imports")
 
 
 # Analisis opcion 2 tipo de transporte
-df_total_transport = df.groupby("transport_mode")[["total_value"]].sum().reset_index()
-df_top_transport = df_total_transport.nlargest(3, "total_value")
+df_top_transport_fn = top_values(df, 3, "transport_mode", "total_value")
+df_top_transport_fn_x = top_values(df, 3, "transport_mode", "total_value", "direction", "Exports")
+df_top_transport_fn_i = top_values(df, 3, "transport_mode", "total_value", "direction", "Imports")
 
-
-df_total_transport_x = df[df["direction"] == "Exports"].groupby("transport_mode")[["total_value"]].sum().reset_index()
-df_top_transport_x = df_total_transport_x.nlargest(3, "total_value")
-
-
-df_total_transport_i = df[df["direction"] == "Imports"].groupby("transport_mode")[["total_value"]].sum().reset_index()
-df_top_transport_i = df_total_transport_i.nlargest(3, "total_value")
-
-#print(df_top_transport)
-#print(df_top_transport_x)
 #print(df_top_transport_i)
 
 # Analisis opcion 3 valor de las exportaciones
-df_cumsum_country = df.groupby("destination")["total_value"].sum().sort_values(ascending=False).reset_index()
-#print(df_cumsum_country)
-
-df_cumsum_country["cum_percent"] = 100*(df_cumsum_country["total_value"].cumsum()/df_cumsum_country["total_value"].sum())
-print(df_cumsum_country)
+df_cumsum_country_fn = market_share(df, "destination", "total_value", 80)
+print(df_cumsum_country_fn)
